8ad5cd217d4cf01bb9597bd694bae40c9973746f
Redouan Cherrat <recherra@c2r4p9.1337.ma>
2023-12-10 19:00:21 +0100

    preprod

---
+++ ft_atoi.c
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: recherra <recherra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 18:43:53 by recherra          #+#    #+#             */
+/*   Updated: 2023/12/10 18:58:12 by recherra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_atoi(const char *str)
+{
+	int	number;
+	int	sign;
+
+	number = 0;
+	sign = 0;
+	while ((*str >= 9 && *str <= 13) || *str == 32)
+		str++;
+	while (*str == '+' || *str == '-')
+	{
+		if (*(str + 1) == '+' || *(str + 1) == '-')
+			return (0);
+		else if (*str == '-')
+			sign = 1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		number = number * 10;
+		number = number + (*str - 48);
+		str++;
+	}
+	if (sign == 1)
+		return (-number);
+	return (number);
+}
--- libft.h
+++ libft.h
@@ -1,18 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: recherra <recherra@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/10 18:40:06 by recherra          #+#    #+#             */
+/*   Updated: 2023/12/10 18:40:23 by recherra         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef LIBFT_H
-#define LIBFT_H
+# define LIBFT_H
 
-#include <stdio.h>
+# include <stdio.h>
 
-int ft_isalpha(int c);
-int ft_isdigit(int c);
-int ft_isalnum(int c);
-int ft_isascii(int c);
-int ft_isprint(int c);
-size_t  ft_strlen(const char *s);
-void    *ft_memset(void *b, int c, size_t len);
-int ft_toupper(int c);
-int ft_tolower(int c);
-char    *ft_strchr(const char *s, int c);
-int ft_atoi(const char *str);
+int		ft_isalpha(int c);
+int		ft_isdigit(int c);
+int		ft_isalnum(int c);
+int		ft_isascii(int c);
+int		ft_isprint(int c);
+size_t	ft_strlen(const char *s);
+int		ft_toupper(int c);
+int		ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+int		ft_atoi(const char *str);
 
-#endif 
+#endif
