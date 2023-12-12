1fabe3a1a26daa5192a0a4dc7b85cb37ba503436
Redouan Cherrat <recherra@c2r4p9.1337.ma>
2023-12-12 21:46:15 +0100

    preprod

--- ft_memchr.c
+++ ft_memchr.c
@@ -1,23 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_memchr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: recherra <recherra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/12 14:58:28 by recherra          #+#    #+#             */
-/*   Updated: 2023/12/12 21:40:04 by recherra         ###   ########.fr       */
+/*   Created: 2023/12/12 20:58:26 by recherra          #+#    #+#             */
+/*   Updated: 2023/12/12 21:45:44 by recherra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-int	main(void)
+//if c does not exist while n in s return (null);
+//if c == \0 && n != strlen(s) return (null); else
+// return \0
+void	*ft_memchr(const void *str, int c, size_t n)
 {
-	const char *s = "jackass";
-	printf("\noriginal %s\n", memchr(s, 'z', sizeof s));
-	printf("mimic %s\n", ft_memchr(s, 'z', sizeof s));
+	unsigned long	len;
+	unsigned char	*s;
+	unsigned char	ac;
+
+	len = ft_strlen(str);
+	s = (unsigned char *)str;
+	ac = (unsigned char)c;
+	while (*s && n--)
+	{
+		if (*s == ac)
+			return (s);
+		s++;
+	}
+	if (ac == '\0' && n != len)
+		return ((char *)(str + len));
+	return (NULL);
 }
