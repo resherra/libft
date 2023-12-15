ca41d01061b78f2715511d029f44a7dfccf95fab
Redouan Cherrat <recherra@c2r4p9.1337.ma>
2023-12-15 16:25:56 +0100

    preprod

--- ft_strdup.c
+++ ft_strdup.c
@@ -1,23 +1,33 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: recherra <recherra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/12 14:58:28 by recherra          #+#    #+#             */
-/*   Updated: 2023/12/15 16:16:42 by recherra         ###   ########.fr       */
+/*   Created: 2023/12/15 16:02:35 by recherra          #+#    #+#             */
+/*   Updated: 2023/12/15 16:24:19 by recherra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
-int main(void)
+char	*ft_strdup(const char *s1)
 {
-	const char *str = "hello jack";
-	printf("ORIGINAL \n%s\n", strdup(str));
-	printf("MIMIC \n%s\n", ft_strdup(str));
+	char	*str;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = ft_strlen(s1);
+	str = malloc((sizeof(char) * len) + 1);
+	while (s1[i])
+	{
+		str[i] = s1[i];
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
 }
--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,37 +6,31 @@
 /*   By: recherra <recherra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:45:45 by recherra          #+#    #+#             */
-/*   Updated: 2023/12/15 16:01:05 by recherra         ###   ########.fr       */
+/*   Updated: 2023/12/15 16:02:57 by recherra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-// if to_find == "" || str == "" , return (str);
-// if len == 0 , return (null);
-
-// int ft_strncmp(const char *s1, const char *s2, size_t n);
-// size_t ft_strlen(const char *s);
-
-char	*ft_strnstr(const char *str, const char *to_find, size_t len)
+char *ft_strnstr(const char *str, const char *to_find, size_t len)
 {
-	int	i;
-	int	is_diff;
+  int i;
+  int is_diff;
 
-	i = 0;
-	if (!ft_strlen(to_find))
-		return ((char *)str);
-	while (str[i] && len)
-	{
-		if ((str[i] == to_find[0]) && ft_strlen(to_find) <= len)
-		{
-			is_diff = ft_strncmp(str + i, to_find, ft_strlen(to_find));
-			if (is_diff == 0)
-				return ((char *)(str + i));
-		}
-		i++;
-		len--;
-	}
-	return (NULL);
+  i = 0;
+  if (!ft_strlen(to_find))
+    return ((char *)str);
+  while (str[i] && len)
+  {
+    if ((str[i] == to_find[0]) && ft_strlen(to_find) <= len)
+    {
+      is_diff = ft_strncmp(str + i, to_find, ft_strlen(to_find));
+      if (is_diff == 0)
+        return ((char *)(str + i));
+    }
+    i++;
+    len--;
+  }
+  return (NULL);
 }
