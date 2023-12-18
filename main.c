0a78ee9bbd8084e518590f594e9de909c7c380d1
Redouan Cherrat <recherra@c2r4p9.1337.ma>
2023-12-18 13:19:15 +0100

    preprod

--- main.c
+++ main.c
@@ -1,13 +1,10 @@
 #include "libft.h"
 
-int	main(void)
+int main(void)
 {
-	char	s1[];
-	char	*set;
-
-	s1[] = "  \t \t \n   \n\n\n\t ";
-	set = " \n\t";
-	// char s1[] = "heredouanhe";
-	// char *set = "he";
-	printf("\n|%s|\n", ft_strtrim(s1, set));
+    char s1[] = "  \t \t \n   \n\n\n\t ";
+    char set[] = " \n\t";
+    // char s1[] = "heredouanhe";
+    // char *set = "he";
+    printf("\n|%s|\n", ft_strtrim(s1, set));
 }
