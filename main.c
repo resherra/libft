b05d43a73f8f28da7fd352ced256da07eae6daa6
Redouan Cherrat <recherra@c2r4p9.1337.ma>
2023-12-18 13:35:01 +0100

    preprod

--- main.c
+++ main.c
@@ -2,9 +2,7 @@
 
 int main(void)
 {
-    char s1[] = "  \t \t \n   \n\n\n\t ";
-    char set[] = " \n\t";
-    // char s1[] = "heredouanhe";
-    // char *set = "he";
+    char s1[] = "heredouanhe";
+    char *set = "he";
     printf("\n|%s|\n", ft_strtrim(s1, set));
 }
