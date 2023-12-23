915d1a409719d58a1c65e8063e8dd7704380ec6c
Redouan Cherrat <recherra@c2r4p9.1337.ma>
2023-12-23 18:59:57 +0100

    preprod

--- ft_lstadd_front_bonus.c
+++ ft_lstadd_front_bonus.c
@@ -1,35 +1,19 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstadd_back_bonus.c                             :+:      :+:    :+:   */
+/*   ft_lstadd_front_bonus.c                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: recherra <recherra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/12/22 12:11:13 by recherra          #+#    #+#             */
-/*   Updated: 2023/12/23 12:39:33 by recherra         ###   ########.fr       */
+/*   Created: 2023/12/23 13:46:08 by recherra          #+#    #+#             */
+/*   Updated: 2023/12/23 18:59:09 by recherra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// typedef struct s_list
-// {
-//     void *content;
-//     struct s_list *next;
-// } t_list;
-
 #include "libft.h"
 
-void ft_lstadd_back(t_list **lst, t_list *new)
+void ft_lstadd_front(t_list **lst, t_list *new)
 {
-    // new->content = "jack";
-    // new->next = NULL;
-
-    t_list *curr = *lst;
-    if (!(*lst))
-    {
-        new->next = NULL;
-        *lst = new;
-    }
-    while (curr != NULL)
-        curr = curr->next;
-    curr->next = new;
+    new->next = *lst;
+    *lst = new;
 }
--- main.c
+++ main.c
@@ -1,37 +1,37 @@
 #include "libft.h"
 
-// int main(void)
-// {
-//     t_list *root = malloc(sizeof(t_list));
-//     t_list *new_node = malloc(sizeof(t_list));
-//     if (!new_node)
-//         exit(1);
-//     if (!root)
-//         exit(1);
-//     root->content = "hello";
-//     root->next = NULL;
-
-//     t_list *curr = root;
-
-//     ft_lstadd_back(&root, new_node);
-
-//     while (curr != NULL)
-//     {
-//         printf("%s\n", curr->content);
-//         curr = curr->next;
-//     }
-// }
-
-int main()
+int main(void)
 {
-    t_list *root, *elem;
-
-    root = malloc(sizeof(t_list));
-    elem = malloc(sizeof(t_list));
+    t_list *root = malloc(sizeof(t_list));
+    t_list *new_node = malloc(sizeof(t_list));
+    if (!new_node)
+        exit(1);
+    if (!root)
+        exit(1);
     root->content = "hello";
-    root->next = elem;
-    root->next->content = "jack";
-    root->next->next = NULL;
+    root->next = NULL;
 
-    printf("%d\n", ft_lstsize(root));
+    t_list *curr = root;
+
+    ft_lstadd_back(&curr, new_node);
+
+    while (curr != NULL)
+    {
+        printf("%s\n", curr->content);
+        curr = curr->next;
+    }
 }
+
+// int main()
+// {
+//     t_list *root, *elem;
+
+//     root = malloc(sizeof(t_list));
+//     elem = malloc(sizeof(t_list));
+//     root->content = "hello";
+//     root->next = elem;
+//     root->next->content = "jack";
+//     root->next->next = NULL;
+
+//     printf("%d\n", ft_lstsize(root));
+// }
