8d558fc21a3c6e7a603fcbeacbc8e04e3e4109aa
resherra <redouancherrat@gmail.com>
2024-01-05 22:56:35 +0100

    preprod

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,37 +6,35 @@
 /*   By: resherra <resherra@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/15 14:45:45 by recherra          #+#    #+#             */
-/*   Updated: 2024/01/05 16:08:58 by resherra         ###   ########.fr       */
+/*   Updated: 2024/01/05 22:45:33 by resherra         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+#include <stdio.h>
 
 char	*ft_strnstr(const char *str, const char *to_find, size_t len)
 {
 	int		i;
 	int		is_diff;
 	size_t	tf_len;
-	char	*s;
-	char	*tf;
 
+	i = 0;
+	tf_len = ft_strlen(to_find);
+	if (!tf_len)
+		return ((char *)(str));
 	if (!str && !len)
 		return (NULL);
-	s = (char *)str;
-	tf = (char *)to_find;
-	tf_len = ft_strlen(tf);
-	i = 0;
-	if (!tf_len)
-		return (s);
-	while (s[i] && len)
+	while (str[i] && len)
 	{
-		if ((s[i] == tf[0]) && tf_len <= len)
+		if ((str[i] == to_find[0]) && tf_len <= len)
 		{
-			is_diff = ft_strncmp(s + i, tf, tf_len);
+			is_diff = ft_strncmp(str + i, to_find, tf_len);
 			if (is_diff == 0)
-				return (s + i);
+				return ((char *)(str + i));
 		}
 		i++;
 		len--;
 	}
+	return (NULL);
 }
